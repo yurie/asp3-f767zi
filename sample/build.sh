9301cbcc71dd2047dccce2c191cb948eca4c858f
yuri <yamayuri@gmail.com>
2020-08-10 16:40:18 +0900

    mrbc実行用スクリプト

---
+++ sample/build.sh
@@ -0,0 +1,4 @@
+rm ./main_task_rb.h
+
+/Users/yuri/work/mruby/grant/static-symbols-test/mruby-fk/bin/mrbc -g -v -Bcode -o./main_task_rb.h ./main_task.rb
+
---
+++ sample/build2.sh
@@ -0,0 +1,4 @@
+rm ../sample/main_task_rb.h
+
+/Users/yuri/work/mruby/grant/static-symbols-test/mruby-fk/bin/mrbc -g -v -Bcode -o../sample/main_task_rb.h ../sample/main_task.rb
+
