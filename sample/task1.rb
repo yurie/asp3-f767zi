ab272d92c51635787502b9359010d628140a3120
yuri <yamayuri@gmail.com>
2020-08-17 10:11:49 +0900

    とりあえず動いた4

--- sample/task1.rb
+++ sample/task1.rb
@@ -17,36 +17,36 @@ while true
   if command
     case command
     when 101 #e
-#      logger.syslog("tsk1:e ext_tsk\r\n")
+      logger.syslog("tsk1:e ext_tsk\r\n")
       # ext_tsk
 #      Nucleo::Task.exit()
     when 115 #s
-#      logger.syslog("tsk1:s slp_tsk\r\n")
+      logger.syslog("tsk1:s slp_tsk\r\n")
       # slp_tsk
 #      Nucleo::Task.sleep()
     when 83 #S
-#      logger.syslog("tsk1:S tslp_tsk(10000000)\r\n")
+      logger.syslog("tsk1:S tslp_tsk(10000000)\r\n")
       # tslp_tsk(10000000)
 #      Nucleo::Task.sleep(10000)
     when 100 #d
-#      logger.syslog("tsk1:d dly_tsk(10000000)\r\n")
+      logger.syslog("tsk1:d dly_tsk(10000000)\r\n")
       # dly_tsk(10000000)
 #      Nucleo::Task.delay(10000)
     when 121 #y
-#      logger.syslog("tsk1:y dis_ter\r\n")
+      logger.syslog("tsk1:y dis_ter\r\n")
       # dis_ter
 #      Nucleo::Task.disable_terminate()
     when 89 #Y
-#      logger.syslog("tsk1:Y ena_ter\r\n")
+      logger.syslog("tsk1:Y ena_ter\r\n")
       # ena_ter
 #      Nucleo::Task.enable_terminate()
     else
       #NucleoではCPUEXXC1未定義のためz,Zもなし
-#      logger.syslog("Unknown tsk1: #{command}\r\n")
+      logger.syslog("Unknown tsk1: #{command}\r\n")
     end
   else
 #    logger.syslog("tsk1: Nil\r\n")
-    loop.consume_time_task_loop
+#    loop.consume_time_task_loop
   end
 #    Nucleo::Task.delay(100000)
 GC.start
