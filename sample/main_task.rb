36fd8583d569737e49c774533fef334282c06911
yuri <yamayuri@gmail.com>
2020-08-17 22:49:37 +0900

    ゴミ掃除

--- sample/main_task.rb
+++ sample/main_task.rb
@@ -3,17 +3,17 @@ PORT_ID = 1
 @logger = Nucleo::Serial.new(PORT_ID)
 @clock = Nucleo::Clock.new()
 loop = Nucleo::Sample1.new()
-@logger.syslog("$$ main task initialize: #{Nucleo::TASK1_ID}\r\n")
+@logger.syslog("$$ main task initialize: #{Nucleo::TASK1_ID}")
 task1_que = Nucleo::DataQue.new(Nucleo::DATA_QUE1_ID)
 
 # タスクの起動
 Nucleo::Task.active(Nucleo::TASK1_ID)
 #Nucleo::Task.active(1)
-@logger.syslog("-- main task task1 active\r\n")
+@logger.syslog("$$ main task task1 active")
 #TODO Nucleo::Task.active(Nucleo::TASK2_ID)
-#TODO log("$$ main task task2 active\r\n")
+#TODO log("$$ main task task2 active")
 #TODO Nucleo::Task.active(Nucleo::TASK3_ID)
-#TODO log("$$ main task task3 active\r\n")
+#TODO log("$$ main task task3 active")
 
 command = 0
 i = 0
@@ -21,203 +21,104 @@ i = 0
 
 while true
   command = @logger.read_byte(PORT_ID)
-  @logger.syslog("read: #{command}\r\n")
+  @logger.syslog("read: #{command}")
 #  case command.chr  #TODO mruby-string-ext を追加するとload irep error になる
   case command
   when 81 #Q Quit
-    @logger.syslog("main: Q\r\n")
+    @logger.syslog("main: Q")
     break
-#  when "e", "s", "S", "d", "y", "Y", "z", "Z" #e, s, S, d, y, Y, z, Z
   when 101, 115, 83, 100, 121, 89, 122, 90 #e, s, S, d, y, Y, z, Z
-    @logger.syslog("main:esSdyYzZ\r\n")
+    @logger.syslog("main:esSdyYzZ")
     case @task_no
     when Nucleo::TASK1_ID
       task1_que.force_send(command)
-#      @logger.syslog("que1: #{command}\r\n")
-    when 20
-    when 30
+#      @logger.syslog("que1: #{command}")
+#TODO    when Nucleo::TASK2_ID
+#TODO    when Nucleo::TASK3_ID
     else
     end
   when 66 #B
-    @logger.syslog("main: B \r\n")
-#    @logger.syslog("main: B:stp_alm(ALMHDR1)")
-#    syslog(LOG_INFO, "#stp_alm(ALMHDR1)");
-#    SVC_PERROR(stp_alm(ALMHDR1));
-##    Nucleo::Task.stop_alarm(Nucleo::ALMHDR1)
+    @logger.syslog("main: B")
+#TODO    Nucleo::Task.stop_alarm(Nucleo::ALMHDR1)
   when 98 #b
-    @logger.syslog("main: b \r\n")
-#    @logger.syslog("main: b:sta_alm(ALMHDR1, 5000000)")
-#    syslog(LOG_INFO, "#sta_alm(ALMHDR1, 5000000)");
-#    SVC_PERROR(sta_alm(ALMHDR1, 5000000));
-##  Nucleo::Task.start_alarm(Nucleo::ALMHDR1, 5000)
+    @logger.syslog("main: b")
+#TODO  Nucleo::Task.start_alarm(Nucleo::ALMHDR1, 5000)
 when 108 #l
-    @logger.syslog("main: l \r\n")
-#    @logger.syslog("main: l:rel_wai(#{@task_no})")
-#    syslog(LOG_INFO, "#rel_wai(%d)", tskno);
-#    SVC_PERROR(rel_wai(tskid));
-    if @task_no != 0
-##      Nucleo::Task.stop_alarm(@task_no)
-    end
+    @logger.syslog("main: l")
+#TODO      Nucleo::Task.stop_alarm(@task_no)
   when 86 #V 高分解能タイマを読むテスト。mrubyではひとまず実装しない
-    @logger.syslog("main: V \r\n")
-#    hrtcnt1 = fch_hrt();
-#    consume_time(1000LU);
-#    hrtcnt2 = fch_hrt();
-#    syslog(LOG_NOTICE, "hrtcnt1 = %tu, hrtcnt2 = %tu",
-#              (uint32_t) hrtcnt1, (uint32_t) hrtcnt2);
+    @logger.syslog("main: V")
   when 120 #x
-    @logger.syslog("main: x \r\n")
-#    @logger.syslog("main: x:ras_ter(#{@task_no})")
-#    syslog(LOG_INFO, "#ras_ter(%d)", tskno);
-#    SVC_PERROR(ras_ter(tskid));
-    if @task_no != 0
-##      Nucleo::Task.raise_termination(@task_no)
-    end
+    @logger.syslog("main: x")
+#TODO      Nucleo::Task.raise_termination(@task_no)
   when 110 #m
-    @logger.syslog("main: m \r\n")
-#    @logger.syslog("main: m:rsm_tsk(#{@task_no})")
-#    syslog(LOG_INFO, "#rsm_tsk(%d)", tskno);
-#    SVC_PERROR(rsm_tsk(tskid));
-    if @task_no != 0
-##      Nucleo::Task.resume(@task_no)
-    end
+    @logger.syslog("main: m")
+#TODO      Nucleo::Task.resume(@task_no)
   when 113 #q 発行したシステムコールを表示しない #mrubyでは実装しない
-    @logger.syslog("main: q \r\n")
-#    SVC_PERROR(syslog_msk_log(LOG_UPTO(LOG_NOTICE),
-#    LOG_UPTO(LOG_EMERG)));
+    @logger.syslog("main: q")
   when 114 #r
-    @logger.syslog("main: r \r\n")
-#    @logger.syslog("main: r:rot_rdq(three priorities)")
-#    syslog(LOG_INFO, "#rot_rdq(three priorities)");
-#    SVC_PERROR(rot_rdq(HIGH_PRIORITY));
-#    SVC_PERROR(rot_rdq(MID_PRIORITY));
-#    SVC_PERROR(rot_rdq(LOW_PRIORITY));
-##      Nucleo::Task.rotate_ready_queue(Nucleo::HIGH_PRIORITY)
-##      Nucleo::Task.rotate_ready_queue(Nucleo::MID_PRIORITY)
-##      Nucleo::Task.rotate_ready_queue(Nucleo::LOW_PRIORITY)
+    @logger.syslog("main: r")
+#TODO      Nucleo::Task.rotate_ready_queue(Nucleo::HIGH_PRIORITY)
+#TODO      Nucleo::Task.rotate_ready_queue(Nucleo::MID_PRIORITY)
+#TODO      Nucleo::Task.rotate_ready_queue(Nucleo::LOW_PRIORITY)
   when 119 #w
-    @logger.syslog("main: w \r\n")
-#    @logger.syslog("main: w:wup_tsk(#{@task_no}) \r\n")
-#    syslog(LOG_INFO, "#wup_tsk(%d)", tskno);
-#    SVC_PERROR(wup_tsk(tskid));
-#    if @task_no != 0
-      Nucleo::Task.wakeup(@task_no)
-#    end
+    @logger.syslog("main: w")
+    Nucleo::Task.wakeup(@task_no)
   when 49 #1
     @logger.syslog("main: 1")
     @task_no = Nucleo::TASK1_ID
   when 50 #2
     @logger.syslog("main: 2")
-    @task_no = 2
+#TODO    @task_no = Nucleo::TASK2_ID
   when 51 #3
     @logger.syslog("main: 3")
-    @task_no = 3
+#TODO    @task_no = Nucleo::TASK3_ID
   when 97 #a
-    @logger.syslog("main: a :#{@task_no}\r\n")
-#    @logger.syslog("main: a:act_tsk(#{@task_no})")
-    # ack_tsk(@task_no)
-#    syslog(LOG_INFO, "#act_tsk(%d)", tskno);
-#    SVC_PERROR(act_tsk(tskid));
-#    if @task_no != 0
-      Nucleo::Task.active(@task_no)
-#    end
+    @logger.syslog("main: a :#{@task_no}")
+    Nucleo::Task.active(@task_no)
   when 65 #A
-    @logger.syslog("main: A \r\n")
-#    @logger.syslog("main: A:can_act(#{@task_no})")
-    # can_act
-#    syslog(LOG_INFO, "#can_act(%d)", tskno);
-#    SVC_PERROR(ercd = can_act(tskid));
-#    if (ercd >= 0) {
-#      syslog(LOG_NOTICE, "can_act(%d) returns %d", tskno, ercd);
-#    }
-    if @task_no != 0
-##      Nucleo::Task.cancel_activetion(@task_no)
-    end
+    @logger.syslog("main: A")
+#TODO      result = Nucleo::Task.cancel_activetion(@task_no)
+#TODO      if result >= 0
+#TODO        @logger.syslog("can_act(#{@task_no}) returns #{result}")
+#TODO      end
   when 116 #t
-    @logger.syslog("main: t \r\n")
-#    @logger.syslog("main: t:ter_tsk(#{@task_no})")
-#    syslog(LOG_INFO, "#ter_tsk(%d)", tskno);
-#    SVC_PERROR(ter_tsk(tskid));
-    if @task_no != 0
-##      Nucleo::Task.terminate(@task_no)
-    end
+    @logger.syslog("main: t")
+#TODO      Nucleo::Task.terminate(@task_no)
   when 62 #>
-    @logger.syslog("main: > \r\n")
-#    @logger.syslog("main: >:chg_pri(#{@task_no}, HIGH_PRIORITY)")
-#    syslog(LOG_INFO, "#chg_pri(%d, HIGH_PRIORITY)", tskno);
-#    SVC_PERROR(chg_pri(tskid, HIGH_PRIORITY));
-    if @task_no != 0
-##      Nucleo::Task.change_priority(@task_no, Nucleo::HIGH_PRIORITY)
-    end
+    @logger.syslog("main: >")
+#TODO      Nucleo::Task.change_priority(@task_no, Nucleo::HIGH_PRIORITY)
   when 61 #=
-    @logger.syslog("main: = \r\n")
-#    @logger.syslog("main: =:chg_pri(#{@task_no}, MID_PRIORITY)")
-#    syslog(LOG_INFO, "#chg_pri(%d, MID_PRIORITY)", tskno);
-#    SVC_PERROR(chg_pri(tskid, MID_PRIORITY));
-    if @task_no != 0
-##      Nucleo::Task.change_priority(@task_no, Nucleo::MID_PRIORITY)
-    end
+    @logger.syslog("main: =")
+#TODO      Nucleo::Task.change_priority(@task_no, Nucleo::MID_PRIORITY)
   when 60 #<
-    @logger.syslog("main: < \r\n")
-#    @logger.syslog("main: <:chg_pri(#{@task_no}, LOW_PRIORITY)")
-#    syslog(LOG_INFO, "#chg_pri(%d, LOW_PRIORITY)", tskno);
-#    SVC_PERROR(chg_pri(tskid, LOW_PRIORITY));
-    if @task_no != 0
-##      Nucleo::Task.change_priority(@task_no, Nucleo::LOW_PRIORITY)
-    end
+    @logger.syslog("main: <")
+#TODO      Nucleo::Task.change_priority(@task_no, Nucleo::LOW_PRIORITY)
   when 500 #dummy
     @logger.syslog("dummy")
   when 71 #G
-    @logger.syslog("main: G \r\n")
-#    @logger.syslog("main: G:get_pri(#{@task_no}, &tskpri)")
-#    syslog(LOG_INFO, "#get_pri(%d, &tskpri)", tskno);
-#    SVC_PERROR(ercd = get_pri(tskid, &tskpri));
-#    if (ercd >= 0) {
-#      syslog(LOG_NOTICE, "priority of task %d is %d", tskno, tskpri);
-#    }
-    if @task_no != 0
-##      priority = Nucleo::Task.get_priority(@task_no)
-##      @logger.syslog("getpri: #{priority} \r\n")
-    end
+    @logger.syslog("main: G")
+#TODO      priority = Nucleo::Task.get_priority(@task_no)
+#TODO      @logger.syslog("getpri: #{priority} \r\n")
   when 87 #W
-    @logger.syslog("command: W \r\n")
-#    @logger.syslog("command: W:can_wup(#{@task_no})")
-#    syslog(LOG_INFO, "#can_wup(%d)", tskno);
-#    SVC_PERROR(ercd = can_wup(tskid));
-#    if (ercd >= 0) {
-#      syslog(LOG_NOTICE, "can_wup(%d) returns %d", tskno, ercd);
-#    }
-    if @task_no != 0
-##      err = Nucleo::Task.cancel_wakeup(@task_no)
-##      if err >=0
-##        @logger.syslog("can_wup#{@task_no} returns#{err}\r\n")
-##      end
-    end
+    @logger.syslog("command: W")
+#TODO      err = Nucleo::Task.cancel_wakeup(@task_no)
+#TODO      if err >=0
+#TODO        @logger.syslog("can_wup#{@task_no} returns#{err}\r\n")
+#TODO      end
   when 117 #u
-    @logger.syslog("main: u \r\n")
-#    @logger.syslog("main: u:sus_tsk(#{@task_no})")
-#    syslog(LOG_INFO, "#sus_tsk(%d)", tskno);
-#    SVC_PERROR(sus_tsk(tskid));
-    if @task_no != 0
-##      Nucleo::Task.suspend(@task_no)
-    end
+    @logger.syslog("main: u")
+#TODO      Nucleo::Task.suspend(@task_no)
   when 501 #dummy
     @logger.syslog("dummy")
   when 99 #c
-    @logger.syslog("main: c \r\n")
-#    @logger.syslog("main: c:sta_cyc(CYCHDR1)")
-#    syslog(LOG_INFO, "#sta_cyc(CYCHDR1)");
-#    SVC_PERROR(sta_cyc(CYCHDR1));
-##    Nucleo::Task.start_cyclic(Nucleo::CYCHDR1)
+    @logger.syslog("main: c")
+#TODO    Nucleo::Task.start_cyclic(Nucleo::CYCHDR1)
   when 67 #C
-    @logger.syslog("main: C \r\n")
-#    @logger.syslog("main: C:stp_cyc(CYCHDR1)")
-#    syslog(LOG_INFO, "#stp_cyc(CYCHDR1)");
-#    SVC_PERROR(stp_cyc(CYCHDR1));
-##    Nucleo::Task.stop_cyclic(Nucleo::CYCHDR1)
-  #  when "\003" //TODO
+    @logger.syslog("main: C")
+#TODO    Nucleo::Task.stop_cyclic(Nucleo::CYCHDR1)
+  #  when "\003" #TODO
   else
-#    @logger.syslog("else\r\n")
     @logger.syslog("Unknown: #{command}")
   end
 end
--- sample/task1.rb
+++ sample/task1.rb
@@ -8,43 +8,34 @@ command = 0
 while true
   counter += 1
   command = que.receive_polling
-  logger.syslog("## tsk1 is running #{counter} : #{command}.  |")
+  logger.syslog("task1 is running #{counter} : #{command}.  |")
   loop.consume_time_task_loop
 
   if command
     case command
     when 101 #e
-      logger.syslog("tsk1:e ext_tsk\r\n")
-      # ext_tsk
+      logger.syslog("tsk1:e ext_tsk")
       Nucleo::Task.exit()
     when 115 #s
-      logger.syslog("tsk1:s slp_tsk\r\n")
-      # slp_tsk
+      logger.syslog("tsk1:s slp_tsk")
       Nucleo::Task.sleep()
     when 83 #S
-      logger.syslog("tsk1:S tslp_tsk(10000000)\r\n")
-      # tslp_tsk(10000000)
+      logger.syslog("tsk1:S tslp_tsk(10000000)")
       Nucleo::Task.sleep(10000)
     when 100 #d 10sは長いので5sにする
-      logger.syslog("tsk1:d dly_tsk(5000000)\r\n")
-      # dly_tsk(5000000)
+      logger.syslog("tsk1:d dly_tsk(5000000)")
       Nucleo::Task.delay(5000)
     when 121 #y
-      logger.syslog("tsk1:y dis_ter\r\n")
-      # dis_ter
-#      Nucleo::Task.disable_terminate()
+      logger.syslog("tsk1:y dis_ter")
+#TODO      Nucleo::Task.disable_terminate()
     when 89 #Y
-      logger.syslog("tsk1:Y ena_ter\r\n")
+      logger.syslog("tsk1:Y ena_ter")
       # ena_ter
-#      Nucleo::Task.enable_terminate()
+#TODO      Nucleo::Task.enable_terminate()
     else
       #NucleoではCPUEXXC1未定義のためz,Zもなし
-      logger.syslog("Unknown tsk1: #{command}\r\n")
+      logger.syslog("Unknown tsk1: #{command}")
     end
-  else
-#    logger.syslog("tsk1: Nil\r\n")
-#    loop.consume_time_task_loop
   end
-#    Nucleo::Task.delay(100000)
 GC.start
 end
